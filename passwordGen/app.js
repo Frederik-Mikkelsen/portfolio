9b254450afe679a1b975deb75ea3e27a95445ddd
Frederik Mikkelsen <Fmarbejde@gmail.com>
2022-12-01 01:10:49 +0100

    password generator app commit
    password generator app

---
+++ passwordGen/app.js
@@ -0,0 +1,26 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N",
+"O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g",
+"h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", 
+"0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%",
+"^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",
+".","?","/"];
+
+
+button = document.getElementById('button')
+passwordBoxOne = document.getElementById('passwordBoxOne') 
+passwordBoxTwo = document.getElementById('passwordBoxTwo') 
+
+
+function generatePasswords() {
+    passwordBoxOne.textContent = ""
+    passwordBoxTwo.textContent = ""
+    for (let i = 0; i < 15; i++) {
+            let randomIndexOne = Math.floor( Math.random() * characters.length )
+            let randomIndexTwo = Math.floor( Math.random() * characters.length )
+            
+            passwordBoxOne.textContent += characters[randomIndexOne]
+            passwordBoxTwo.textContent += characters[randomIndexTwo]
+        
+    }
+}
+
